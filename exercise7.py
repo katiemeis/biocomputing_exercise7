a5046da14db32f465dd60cf702b557e4e4d1ffdc
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-10-12 12:42:44 -0400

    finished assignment

--- exercise7.py
+++ exercise7.py
@@ -1,4 +1,4 @@
-#BIOCOMPUTING EXERSIZE 7 CODE 
+#BIOCOMPUTING EXERSIZE 7 
 import pandas as pd 
 data=pd.read_csv('iris.csv',header=0,sep=',')
 
@@ -12,11 +12,11 @@ oddrows(data)
 
 #Task 2a: function that return the number of observations for a given species included in the data set
 def sp_obs(variable,species):
-    species=variable[data.Species=='species']
+    species=variable.loc[variable.Species==species]
     length=len(species)
     
     return length 
-
+   
 sp_obs(data,'setosa')
 
 #Task 2b: dataframe for flowers with Sepal.Width greater than a value specified by the function user
@@ -27,15 +27,15 @@ def Sepal_Width(variable,n):
 
 Sepal_Width(data,3.5)
     
-#Task 2c: data for a given species to a comma-delimited file with the given species name as the file name; Hint: look at using + or % to concatenate your species name and “.csv”
+#Task 2c: data for a given species to a comma-delimited file with the given species name as the file name
+def species(variable, name):
+    sp=variable.loc[variable['Species']==name]
+    sp.to_csv(path_or_buf=name+'.csv')
+    return sp
 
-
-
-
-
-
-
-
+species(data, 'virginica')
+species(data,'setosa')
+species(data,'versicolor')
 
 
 
