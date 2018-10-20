8f44ee789643799d802b7cbfc2eee365e26710e0
Leigh C Campbell <lcampbe3@remote201.helios.nd.edu>
2018-10-20 19:32:07 -0400

    finished

---
+++ assignment7.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 16 15:59:55 2018
+
+@author: lcampbe3
+
+exercise #7
+"""
+
+#1 - print a dataframe odd rows
+import pandas as pd
+import numpy as np
+def odds(df):
+    counter=0
+    for i in range(0, len(df)):
+        if(counter%2 != 0):
+            print df.iloc[i]
+        counter += 1
+            
+test=pd.DataFrame(np.random.rand(10,4), columns=list('ABCD'))
+odds(test)
+
+#2
+#number of observations for a given species
+def species_counts(df, species):
+    sub_spec = df.loc[df["Species"] == species]
+    sp_count = sub_spec["Species"].value_counts().to_frame()
+    return sp_count
+
+#test the function
+iris_data = pd.read_csv("iris.csv", sep = ",")
+species_counts(iris_data, "setosa")
+
+#dataframe for specific width
+def width(df, wth):
+    row = df.loc[df["Sepal.Width"] > wth]
+    return row
+
+#test the function
+width(iris_data, 3.5)
+
+#function for given species -> file
+def species_file(df, species):
+    sp = df.loc[df["Species"] == species]
+    str1 = ".csv"
+    species += str1
+    sp.to_csv(species, sep=",")
+
+#test the function
+species_file(iris_data, "setosa")
+
+
